1f8c3dbcf17acc1a5616e3d46fb8ad97a6cd097b
Wil <wilmerulcuevelasco@gmail.com>
2025-06-25 17:32:05 -0500

    Se estable la estructura de carpetas con lo requerido para el portafolio

---
+++ src/pages/Store/Store.jsx
@@ -0,0 +1,10 @@
+const Store = () => {
+    return (
+        <div className="store">
+            <h1>Store Page</h1>
+            <p>Welcome to the store! Here you can find various products.</p>
+            {/* Add more content or components related to the store here */}
+        </div>
+    )
+}
+export default Store
